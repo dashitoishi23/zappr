0f8323a39e720421ebd3d6095c1885b432b4f232
Hitoishi Das <hitoishi.d@technovert.com>
2022-05-12 17:08:47 +0530

    Added strongly typed endpoint struct:

--- pkg/user/endpoints/endpoints.go
+++ pkg/user/endpoints/endpoints.go
@@ -1,6 +1,9 @@
 package userservice
 
 import (
+	"context"
+	"errors"
+
 	userservice "dev.azure.com/technovert-vso/Zappr/_git/Zappr/pkg/user/service"
 	"github.com/go-kit/kit/endpoint"
 )
@@ -9,4 +12,22 @@ type Set struct {
 	GenerateToken endpoint.Endpoint
 } //defines all endpoints as having type Endpoint, provided by go-kit
 
-func GenerateToken(s userservice.UserService)
+func GenerateToken(s userservice.UserService) endpoint.Endpoint{
+	return func(ctx context.Context, request interface{}) (response interface{}, err error){
+		_, ok:= request.(GenerateTokenRequest)
+		s := s.GenerateToken(ctx)
+		if !ok {
+			err:= errors.New("Invalid request")
+		}
+		return GenerateTokenResponse{s: s, err: err}, nil
+	}
+}
+
+type GenerateTokenRequest struct {
+
+} //strongly typed request object
+
+type GenerateTokenResponse struct {
+	s string `json:"s"`
+	err error `json:"err"`
+} //strongly typed response object
