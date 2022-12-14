84ac4d4503a085e892b5caf3dcd13ea85c859267
Hitoishi Das <hitoishi.d@technovert.com>
2022-12-14 15:53:45 +0530

    Connection validation check wip

--- pkg/initsetup/service/service.go
+++ pkg/initsetup/service/service.go
@@ -2,12 +2,17 @@ package initsetupservice
 
 import (
 	"encoding/json"
+	"fmt"
 	"reflect"
+	"strconv"
 
 	initsetupmodels "dev.azure.com/technovert-vso/Zappr/_git/Zappr/pkg/initsetup/models"
 	repository "dev.azure.com/technovert-vso/Zappr/_git/Zappr/repository"
 	util "dev.azure.com/technovert-vso/Zappr/_git/Zappr/util"
 	"github.com/gomodule/redigo/redis"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 type InitSetupService interface {
@@ -33,6 +38,18 @@ func (i *initSetupService) AddConfig(newConfig initsetupmodels.Config) (initsetu
 
 	if err != nil {
 		return initsetupmodels.Config{}, err
+	}
+
+	errString:= isDBConnectionValid(newConfig)
+
+	if errString != "Valid"{
+		return initsetupmodels.Config{}, fmt.Errorf(errString)
+	}
+
+	errString = isRedisConnectionValid(newConfig)
+
+	if errString != "Valid"{
+		return initsetupmodels.Config{}, fmt.Errorf(errString)
 	}
 
 	dbConfig.InitFields()
@@ -85,4 +102,81 @@ func populateDBConfig(newConfig initsetupmodels.Config) (initsetupmodels.DBConfi
 		Config: json.RawMessage(encodedJson),
 	}, nil
 
+}
+
+func isDBConnectionValid(newConfig initsetupmodels.Config) string {
+	dsn := dsnBuilder(newConfig.DatabaseUser, newConfig.DatabasePassword, newConfig.DatabaseName, newConfig.DatabaseHost, 5432, newConfig.DatabaseSSLMode)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+	SkipDefaultTransaction: true,
+	NamingStrategy: schema.NamingStrategy{
+		SingularTable: true,
+		NoLowerCase: true,
+		},
+	})
+
+	if err != nil{
+		return err.Error()
+	}
+
+	database, err := db.DB()
+
+	if err != nil {
+		return err.Error()
+	}
+
+	err = database.Ping()
+
+	if err != nil {
+		return err.Error()
+	}
+
+	defer database.Close()
+
+	return "Valid"
+}
+
+func isRedisConnectionValid(newConfig initsetupmodels.Config) string {
+	pool := &redis.Pool{
+		MaxIdle: 80,
+		MaxActive: 0,
+		Dial: func() (redis.Conn, error) {
+
+			if newConfig.RedisHost == "" {
+				newConfig.RedisHost = "redis"
+			}
+
+			c, err := redis.Dial("tcp", newConfig.RedisHost + ":6379")
+			
+			if err != nil {
+				panic(err)
+			}
+
+			if newConfig.RedisPassword != "" {
+				_, authErr := c.Do("AUTH", newConfig.RedisPassword)
+
+				if authErr != nil {
+				panic(authErr)
+			}
+			}
+
+			return c, err
+
+		},
+	}
+
+	defer pool.Close()
+
+	err := pool.Get().Err()
+
+	if err != nil {
+		return err.Error()
+	}
+
+	return "Valid"
+
+}
+
+func dsnBuilder(user string, password string, dbName string, host string, port int, sslMode string) string {
+	return fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=%s", host, strconv.Itoa(port), dbName, user, password, sslMode)
 }
